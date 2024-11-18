d44010dd59586f4f57e4bd249b62583c7f8f65a0
e.liu11 <e.liu2808@gmail.com>
2024-11-18 11:24:10 -0700

    11/18/2024

---
+++ src/As1_Crops/As1_Crop.java
@@ -0,0 +1,26 @@
+package As1_Crops;
+
+public class As1_Crop {
+    private String name;
+    private double yield;
+    private String units;
+    private double price;
+    private int acres;
+
+    public As1_Crop( String n, double y, String u, double p ){
+        name = n;
+        yield = y;
+        units = u;
+        price = p;
+        acres = 0;
+    }
+
+    public String toString(){
+        return name + ", yield: " + yield + ", units: " + units + ", price: " + price + ", acres: " + acres;
+    }
+
+    public void Setter(){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //Ex1_ArrayLists.run();
         //Examples.Ex2_Main.run();
-        Examples.Ex3_LawnMain.run();
+        //Examples.Ex3_LawnMain.run();
+        As1_Crops.As1_Main.run();
     }
 }
