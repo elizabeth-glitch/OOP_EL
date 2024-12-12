8ed5907da700ad1a3416115fcb444b8a53b88e50
elizabeth-glitch <76456188+elizabeth-glitch@users.noreply.github.com>
2024-12-11 21:44:08 -0700

    Update As2_LeagueMain.java

--- src/As2/As2_LeagueMain.java
+++ src/As2/As2_LeagueMain.java
@@ -12,9 +12,28 @@ public class As2_LeagueMain {
         Scanner input = new Scanner(System.in);
         loadFile("data/TeamsData.csv", allTeams);
         allTeams.add(  new As2_Team( "Los Angeles Kings", "Los Angeles", "Western", "Pacific", 2014, 2, 1967 )  );
-
-        allTeams.allPlayers.addPlayer();
-
+        //player name, ID, goalsScored, points, salary
+        allTeams.get(0).addPlayer("Connor McDavid", 97, 14, 38, 12500000.00);
+        allTeams.get(0).addPlayer("Ryan Nugent-Hopkins", 93, 4, 14, 5125000.00);
+        allTeams.get(0).addPlayer("Darnell Nurse", 25, 3, 11, 9250000.00);
+        allTeams.get(0).addPlayer("Leon Draisaitl", 29, 20, 38, 8500000.00);
+        allTeams.get(0).addPlayer("Evan Bouchard", 2, 6, 20, 3900000.00);
+        allTeams.get(1).addPlayer("Jonathan Huberdeau", 10, 10, 19, 10500000.00);
+        allTeams.get(1).addPlayer("Rasmus Andersson", 4, 6, 15, 4550000.00);
+        allTeams.get(1).addPlayer("Daniil Miromanov", 62, 1, 6, 1250000.00);
+        allTeams.get(1).addPlayer("Andrei Kuzmenko", 96, 1, 10, 5500000.00);
+        allTeams.get(1).addPlayer("Justin Kirkland", 58, 2, 8, 775000.00);
+        allTeams.get(5).addPlayer("Auston Matthews", 34, 0, 0, 775000.00);
+        allTeams.get(5).addPlayer("Mitchell Marner", 16, 0, 0, 0.0);
+        allTeams.get(5).addPlayer("Anthony Stolarz", 41, 0, 0, 0.0);
+        allTeams.get(3).addPlayer("Quinn Hughes", 43, 0, 0, 0.0);
+        allTeams.get(3).addPlayer("Elias Pettersson", 40, 0, 0, 0.0);
+        allTeams.get(3).addPlayer("Thatcher Demko", 35, 0, 0, 0.0);
+        allTeams.get(3).addPlayer("Broch Boeser", 6, 0, 0, 0.0);
+        allTeams.get(8).addPlayer("Matthew Tkachuk", 19, 0, 0, 0.0);
+        allTeams.get(8).addPlayer("Aleksander Barkov", 16, 0, 0, 0.0);
+        allTeams.get(8).addPlayer("Sergei Bobrovsky", 72, 0, 0, 0.0);
+        allTeams.get(8).addPlayer("Sam Reinhart", 13, 0, 0, 0.0);
 
         while(true){
             System.out.println("Menu: ");
@@ -24,6 +43,9 @@ public class As2_LeagueMain {
             System.out.println("4. Sort by Stanley Cup win ");
             System.out.println("5. Update Cup Status");
             System.out.println("6. Exit and save");
+            System.out.println("7. Print players");
+            System.out.println("8. Total Team Player stats");
+            System.out.println("9. Update player stat");
 
             int answer = input.nextInt();
             if(answer == 1){
@@ -133,6 +155,49 @@ public class As2_LeagueMain {
                 saveFile("data/TeamsData.csv", allTeams);
                 break;
             }
+            if(answer == 7){
+                System.out.println("Enter the name of the team: ");
+                input.nextLine();
+                String teamN = input.nextLine();
+                boolean found = false;
+                for(As2_Team team: allTeams){
+                    if(team.getName().equalsIgnoreCase(teamName)){
+                        team.printMyPlayers();
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    System.out.println("Team not found");
+                }
+            }
+            if(answer == 8){
+                System.out.println("Team Goal Scored stats: ");
+                for(As2_Team team : allTeams){
+                    System.out.println(team.getName() + ": " + team.getTotalPlayerStats() + " total goals");
+                }
+            }
+            if(amswer == 9){
+                System.out.println("Enter the name of the player to update");
+                input.nextLine();
+                String playerName = input.nextLine();
+                boolean found = false;
+                for(As2_Team team : allTeams){
+                    for(As3_Player player : team.players){
+                        if(player.getName().equalsIgnoreCase(playerName)){
+                            player.updateStat();
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found){
+                        break;
+                    }
+                }
+                if(!found){
+                    System.out.println("Player not found");
+                }
+            }//9
         }//while
     }//run
 
